d0a4c5ca5444a2d3f036114366a781081d1b43c0
Red Theory <darchie@MacBook-Pro.attlocal.net>
2020-10-08 16:30:02 -0400

    whats-app commit

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAJFle58qQiaBSMLnJ6EfSVd-FAuTUQ5bU",
+  authDomain: "whatsup-clone-14a41.firebaseapp.com",
+  databaseURL: "https://whatsup-clone-14a41.firebaseio.com",
+  projectId: "whatsup-clone-14a41",
+  storageBucket: "whatsup-clone-14a41.appspot.com",
+  messagingSenderId: "615198217487",
+  appId: "1:615198217487:web:3d34b4dbe6beb246a39a7a",
+  measurementId: "G-30XH3XVHF8"
+};
